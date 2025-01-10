76a2aedbea131bfc07846a1448ace49f3546448b
Sammysk12 <samarthkalshetti43@gmail.com>
2025-01-10 12:04:13 +0530

    Inital commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        double a = 9/2;
+
+        System.out.println(a);
+
+
+    }
+}
